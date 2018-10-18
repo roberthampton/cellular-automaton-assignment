17ec71347453585e631afc3e565366acfef4fb01
scarletblade11 <37197505+scarletblade11@users.noreply.github.com>
2018-10-18 13:15:17 +0100

    Add files via upload
    Basic menu structure with converter from decimal to binary.

---
+++ caMenu.c
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void menuRules();
+int randomRule();
+int selectRule();
+void decToBinary(int); 
+
+int main()
+{
+	menuRules();
+}
+
+void menuRules()
+{
+	int choice = 0;
+
+	do
+	{
+		printf("Enter option:\n" );
+		printf("=========================\n" );
+		printf("1.Random Rule\n" );
+		printf("2.Select a Rule\n" );
+		printf("3.Exit\n" );
+		printf("=========================\n" );
+		scanf("%d",&choice);
+
+		switch(choice) {
+
+   		case 1  :
+      		randomRule();
+      		break; /* optional */
+	
+   		case 2  :
+      		selectRule();
+      		break; /* optional */
+
+      	case 3  :
+      	printf("Exiting program :) \n");
+      	break; /* optional */
+  
+   					/* you can have any number of case statements */
+   					default : /* Optional */
+   					break;
+				}
+
+	}while(choice!=3);
+}
+
+int randomRule(){
+	int r = rand() % 256;
+	return r;
+}
+
+int selectRule(){
+
+	int num;
+	printf("Enter a decimal number between 0 and 255\n");
+    scanf("%d",&num);
+    decToBinary(num);
+}
+
+// function to convert decimal to binary 
+void decToBinary(int n) 
+{ 
+    // array to store binary number 
+    int binaryNum[1000]; 
+  
+    // counter for binary array 
+    int i = 0; 
+    while (n > 0) { 
+  
+        // storing remainder in binary array 
+        binaryNum[i] = n % 2; 
+        n = n / 2; 
+        i++; 
+    } 
+  
+    // printing binary array in reverse order 
+    for (int j = i - 1; j >= 0; j--) 
+        printf("%d\n", binaryNum[j]); 
+} 
