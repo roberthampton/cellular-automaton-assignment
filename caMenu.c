dc62a66943eb9be78b2e4cb5a334e2d38e2d4291
Alexander Arnaudov <37197505+scarletblade11@users.noreply.github.com>
2018-10-25 02:17:56 +0100

    Update caMenu.c

--- caMenu.c
+++ caMenu.c
@@ -3,16 +3,21 @@
 
 
 void menuRules();
-int randomRule();
-int selectRule();
-void decToBinary(int); 
 void menuCA();
+void ruleZero();
+void decToBinary(int);
+
+
+char rule[7];
 
 int main()
 {
 	menuRules();
+
+	ruleZero();
 }
 
+//Menu to choose a rule for the program
 void menuRules()
 {
 	int choice = 0;
@@ -32,46 +37,57 @@ void menuRules()
 
    		case 1  :
       		r = rand() % 256;
-			// ca menu  
-      		break; /* optional */
-	
+      		decToBinary(r);
+			// ca menu
+      		break;
+
    		case 2  :
-      		
+
 			printf("Enter a decimal number between 0 and 255\n");
    			scanf("%d",&num);
     		decToBinary(num);
-    		//ca menu 
-      		break; /* optional */
+    		//ca menu
+      		break;
 
       	case 3  :
       	printf("Exiting program :) \n");
-      	break; /* optional */
-  
-   					/* you can have any number of case statements */
-   					default : /* Optional */
+      	break;
+   					default :
    					break;
 				}
 
 	}while(choice!=3);
 }
 
-// function to convert decimal to binary 
-void decToBinary(int n) 
-{ 
-    // array to store binary number 
-    int binaryNum[1000]; 
-  
-    // counter for binary array 
-    int i = 0; 
-    while (n > 0) { 
-  
-        // storing remainder in binary array 
-        binaryNum[i] = n % 2; 
-        n = n / 2; 
-        i++; 
-    } 
-  
-    // printing binary array in reverse order 
-    for (int j = i - 1; j >= 0; j--) 
-        printf("%d\n", binaryNum[j]); 
-} 
+// function to convert decimal to binary
+void decToBinary(int n)
+{
+    // counter for binary array
+    int i = 0;
+    while (n > 0) {
+
+        // storing remainder in binary array
+        rule[i]=n % 2;
+        n = n / 2;
+        i++;
+    }
+
+}
+
+//print rule
+void rulePrint()
+{
+int i;
+    for(i = 0; i<=7;i++)
+    {
+        printf("%d\n", rule[i]);
+    }
+
+}
+
+//menu to work with the cellular automation
+void menuCA()
+{
+
+
+}
